fdd8571894aefee55470b4071f20ed3836d4cff4
gimye <gimyedev@gmail.com>
2025-03-30 22:51:21 +0900

    Docs: 필요한 파일명 외 주석처리 완료

--- efubaccount/src/main/java/com/practice/efubaccount/entity/AccountStatus.java
+++ efubaccount/src/main/java/com/practice/efubaccount/entity/AccountStatus.java
@@ -1,5 +1,5 @@
 package com.practice.efubaccount.entity;
 
 public enum AccountStatus {
-    DEACTIVATED, ACTIVE
+//    DEACTIVATED, ACTIVE
 }
--- efubaccount/src/main/java/com/practice/efubaccount/repository/AccountsRepository.java
+++ efubaccount/src/main/java/com/practice/efubaccount/repository/AccountsRepository.java
@@ -7,10 +7,10 @@ import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Account, Long> {
 
-    // 이메일 중복검사를 위한 쿼리
-    boolean existsByEmail(String email);
-
-    // 회원 ID로 조회
-    Optional<Account> findByAccountId(Long accountId);
+//    // 이메일 중복검사를 위한 쿼리
+//    boolean existsByEmail(String email);
+//
+//    // 회원 ID로 조회
+//    Optional<Account> findByAccountId(Long accountId);
 
 }
