ccfb640d7f2d119eeeb61c3799aaa9186dd68be0
9rin <129544574+9rin@users.noreply.github.com>
2025-09-15 20:45:11 +0900

    feat: 실습 코드 삭제

--- blog/src/main/java/com/practice/blog/account/controller/AccountController.java
+++ blog/src/main/java/com/practice/blog/account/controller/AccountController.java
@@ -2,11 +2,11 @@ package com.practice.blog.account.controller;
 
 import com.practice.blog.account.dto.response.AccountResponseDto;
 import com.practice.blog.account.service.AccountService;
-//import com.practice.blog.account.dto.response.CreateAccountResponseDto;
-//import com.practice.blog.account.dto.request.BioUpdateRequestDto;
-//import com.practice.blog.account.dto.request.CreateAccountRequestDto;
-//import jakarta.validation.Valid;
-//import org.springframework.http.HttpStatus;
+import com.practice.blog.account.dto.response.CreateAccountResponseDto;
+import com.practice.blog.account.dto.request.BioUpdateRequestDto;
+import com.practice.blog.account.dto.request.CreateAccountRequestDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +18,15 @@ public class AccountController {
 
     private final AccountService accountService;
 
-//    // Redis에서 id로 이메일 조회
-//    @GetMapping("/redis/{accountId}")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public String getEmailByIdfromRedis(@PathVariable Long accountId){
-//        return accountService.findEmailByIdFromRedis(accountId);
-//    }
-//
-//    // Mongodb에서  id로 닉네임 조회
-//    @GetMapping("/mongodb/{accountId}")
-//    @ResponseStatus(value = HttpStatus.OK)
-//    public String getNicknameByIdfromMongod(@PathVariable Long accountId){
-//        return accountService.findNicknameByIdFromMongo(accountId);
-//    }
+    // Redis에서 id로 이메일 조회
+
+
+
+
+    // Mongodb에서  id로 닉네임 조회
+
+
+
 
     // 회원 조회: GET /accounts/{accountId}
     @GetMapping("/{accountId}")
@@ -39,20 +35,20 @@ public class AccountController {
         return ResponseEntity.ok(responseDto);
     }
 
-//    // 계정 생성 POST /accounts
-//    @PostMapping
-//    public ResponseEntity<CreateAccountResponseDto> createAccount(@RequestBody @Valid CreateAccountRequestDto requestDto) {
-//        CreateAccountResponseDto responseDto = accountService.createAccount(requestDto);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
-//    }
+    // 계정 생성 POST /accounts
+    @PostMapping
+    public ResponseEntity<CreateAccountResponseDto> createAccount(@RequestBody @Valid CreateAccountRequestDto requestDto) {
+        CreateAccountResponseDto responseDto = accountService.createAccount(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 
-//    // 계정 프로필(자기소개) 수정: PATCH /accounts/profile/{accountId}
-//    @PatchMapping("/profile/{accountId}")
-//    public ResponseEntity<AccountResponseDto> updateAccount(@PathVariable("accountId") Long accountId,
-//                                                            @RequestBody @Valid BioUpdateRequestDto requestDto) {
-//        AccountResponseDto responseDto = accountService.updateAccount(accountId, requestDto);
-//        return ResponseEntity.ok(responseDto);
-//    }
+    // 계정 프로필(자기소개) 수정: PATCH /accounts/profile/{accountId}
+    @PatchMapping("/profile/{accountId}")
+    public ResponseEntity<AccountResponseDto> updateAccount(@PathVariable("accountId") Long accountId,
+                                                            @RequestBody @Valid BioUpdateRequestDto requestDto) {
+        AccountResponseDto responseDto = accountService.updateAccount(accountId, requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
 
     // 계정 논리적 삭제(탈퇴): PATCH /accounts/{accountId}
     @PatchMapping("/{accountId}")
--- blog/src/main/java/com/practice/blog/account/service/AccountService.java
+++ blog/src/main/java/com/practice/blog/account/service/AccountService.java
@@ -1,19 +1,20 @@
 package com.practice.blog.account.service;
 
 import com.practice.blog.account.dto.response.AccountResponseDto;
-//import com.practice.blog.account.dto.response.CreateAccountResponseDto;
-//import com.practice.blog.account.dto.request.BioUpdateRequestDto;
-//import com.practice.blog.account.dto.request.CreateAccountRequestDto;
+import com.practice.blog.account.dto.response.CreateAccountResponseDto;
+import com.practice.blog.account.dto.request.BioUpdateRequestDto;
+import com.practice.blog.account.dto.request.CreateAccountRequestDto;
 import com.practice.blog.account.entity.Account;
-//import com.practice.blog.account.entity.AccountDocument;
 import com.practice.blog.account.entity.AccountStatus;
-//import com.practice.blog.account.repository.AccountDocumentRepository;
 import com.practice.blog.account.repository.AccountsRepository;
+//import com.practice.blog.account.entity.AccountDocument;
+//import com.practice.blog.account.repository.AccountDocumentRepository;
 
 import com.practice.blog.global.exception.BlogException;
 import com.practice.blog.global.exception.ExceptionCode;
-//import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+//import jakarta.annotation.PostConstruct;
+
 //import org.springframework.data.redis.core.HashOperations;
 //import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -29,91 +30,50 @@ public class AccountService {
 
     private final AccountsRepository accountsRepository;
 
-//    private final RedisTemplate<String , Object> redisTemplate; // Redis와 통신
-//    private HashOperations<String , String , Object> hashOperations; // Redis에 저장할 Hash 객체 선언
-//    private static final String ACCOUNT_CACHE_KEY = "Account:";  //key의 접두사
-//
-//    // Mongo DB용 repository
-//    private final AccountDocumentRepository accountDocumentRepository;
-//
-//    // 초기화
-//
-//
-//    // 회원 생성
-//    @Transactional
-//    public CreateAccountResponseDto createAccount(CreateAccountRequestDto requestDto) {
-//        if(accountsRepository.existsByEmail(requestDto.getEmail())) {
-//            throw new IllegalArgumentException("Email already exists");
-//        }
-//        Account account = requestDto.toEntity();
-//        Account savedAccount = accountsRepository.save(account);
-//
-//        // Redis 해시에 이메일과 닉네임 저장
-//
-//
-//        // 만료 시간 설정 (30분)
-//
-//
-//        // MongoDB에 저장
-//
-//        return CreateAccountResponseDto.from(savedAccount);
-//    }
-//
-//
-//
-//    // 회원 수정 (bio, nickname)
-//    @Transactional
-//    public AccountResponseDto updateAccount(Long accountId, BioUpdateRequestDto requestDto) {
-//        Account account = findByAccountId(accountId);
-//        account.updateBio(requestDto.getBio());
-//        account.updateNickname(requestDto.getNickname());
-//
-//        // Redis에서 닉네임 업데이트
-//
-//
-//        // MongoDB에서 닉네임 업데이트
-//
-//        return AccountResponseDto.from(account);
-//    }
-//
-//
-//    // 회원 물리적 삭제
-//    @Transactional
-//    public void physicalDeleteAccount(Long accountId) {
-//        Account account = findByAccountId(accountId);
-//
-//        // Redis에서 삭제
-//
-//
-//        // MySQL에서 삭제
-//        accountsRepository.delete(account);
-//
-//        // MongoDB에서 삭제
-//
-//    }
-//
-//    // Redis에서 ID로 이메일 조회
-//    @Transactional(readOnly = true)
-//    public String findEmailByIdFromRedis(Long id) {
-//
-//
-//        // Redis 해시에서 값 조회
-//
-//            // DB에서 조회
-//
-//
-//            // DB에서 조회한 정보를 Redis에 저장
-//
-//        }
-//
-//    }
-//
-//    // MongoDB에서 ID로 닉네임 조회
-//    @Transactional(readOnly = true)
-//    public String findNicknameByIdFromMongo(Long id) {
-//
-//    }
 
+
+    // 초기화
+
+
+
+    // 회원 생성
+    @Transactional
+    public CreateAccountResponseDto createAccount(CreateAccountRequestDto requestDto) {
+        if(accountsRepository.existsByEmail(requestDto.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        Account account = requestDto.toEntity();
+        Account savedAccount = accountsRepository.save(account);
+        return CreateAccountResponseDto.from(savedAccount);
+    }
+
+
+
+    // 회원 수정 (bio, nickname)
+    @Transactional
+    public AccountResponseDto updateAccount(Long accountId, BioUpdateRequestDto requestDto) {
+        Account account = findByAccountId(accountId);
+        account.updateBio(requestDto.getBio());
+        account.updateNickname(requestDto.getNickname());
+        return AccountResponseDto.from(account);
+    }
+
+
+    // 회원 물리적 삭제
+    @Transactional
+    public void physicalDeleteAccount(Long accountId) {
+        Account account = findByAccountId(accountId);
+        accountsRepository.delete(account);
+    }
+
+    // Redis에서 ID로 이메일 조회
+
+
+
+    // MongoDB에서 ID로 닉네임 조회
+
+
+    /*----------------------------------------------------*/
 
     // 회원 논리적 삭제 (status 변경)
     @Transactional
