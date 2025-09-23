72a510397e7c4af5494255400acad6dd6fc258fd
gimye <gimyedev@gmail.com>
2025-09-23 19:24:32 +0900

    Chore : conflict 해결

--- blog/src/main/java/com/practice/blog/account/service/AccountService.java
+++ blog/src/main/java/com/practice/blog/account/service/AccountService.java
@@ -5,23 +5,24 @@ import com.practice.blog.account.dto.response.CreateAccountResponseDto;
 import com.practice.blog.account.dto.request.BioUpdateRequestDto;
 import com.practice.blog.account.dto.request.CreateAccountRequestDto;
 import com.practice.blog.account.entity.Account;
+import com.practice.blog.account.entity.AccountDocument;
 import com.practice.blog.account.entity.AccountStatus;
+import com.practice.blog.account.repository.AccountDocumentRepository;
 import com.practice.blog.account.repository.AccountsRepository;
-//import com.practice.blog.account.entity.AccountDocument;
-//import com.practice.blog.account.repository.AccountDocumentRepository;
 
 import com.practice.blog.global.exception.BlogException;
 import com.practice.blog.global.exception.ExceptionCode;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-//import jakarta.annotation.PostConstruct;
 
-//import org.springframework.data.redis.core.HashOperations;
-//import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-//import java.util.Map;
-//import java.util.concurrent.TimeUnit;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 
 @Service
 @RequiredArgsConstructor
@@ -29,12 +30,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountService {
 
     private final AccountsRepository accountsRepository;
+    private final AccountDocumentRepository accountDocumentRepository;
 
-
+    private final RedisTemplate<String, Object> redisTemplate; // redis 와 통신
+    private HashOperations<String, String, Object> hashOperations; // redis에 저장할 hash 관련 객체 선언
+    private static final String ACCOUNT_CACHE_KEY = "Account:"; // key의 접두사
 
     // 초기화
-
-
+    @PostConstruct
+    public void init() {
+        this.hashOperations = redisTemplate.opsForHash(); // initialization
+    }
 
     // 회원 생성
     @Transactional
@@ -44,9 +50,26 @@ public class AccountService {
         }
         Account account = requestDto.toEntity();
         Account savedAccount = accountsRepository.save(account);
+
+        // Redis 해시에 이메일과 닉네임 저장
+        String redisKey = ACCOUNT_CACHE_KEY + savedAccount.getAccountId();
+        hashOperations.put(redisKey, "email", savedAccount.getEmail());
+        hashOperations.put(redisKey, "nickname", savedAccount.getNickname());
+
+        // 만료 시간 설정(30분)
+        redisTemplate.expire(redisKey, 30, TimeUnit.MINUTES);
+
+        // MongoDB에 저장
+        AccountDocument accountDocument = AccountDocument.builder()
+                .id(savedAccount.getAccountId().toString())
+                .email(savedAccount.getEmail())
+                .nickname(savedAccount.getEmail())
+                .password(savedAccount.getPassword())
+                .build();
+        accountDocumentRepository.save(accountDocument);
+
         return CreateAccountResponseDto.from(savedAccount);
     }
-
 
 
     // 회원 수정 (bio, nickname)
@@ -55,6 +78,19 @@ public class AccountService {
         Account account = findByAccountId(accountId);
         account.updateBio(requestDto.getBio());
         account.updateNickname(requestDto.getNickname());
+
+        // redis에서 닉네임 업데이트(bio는 저장하지 않으므로 생략)
+        String redisKey = ACCOUNT_CACHE_KEY + accountId;
+        hashOperations.put(redisKey, "nickname", account.getNickname());
+
+        // MongoDB에서 닉네임 업데이트
+        String _id = accountId.toString();
+        AccountDocument accountDocument = accountDocumentRepository.findById(_id)
+                .orElseThrow(()-> new BlogException(ExceptionCode.ACCOUNT_NOT_FOUND));
+
+        accountDocument.update(account.getNickname());
+        accountDocumentRepository.save(accountDocument);
+
         return AccountResponseDto.from(account);
     }
 
@@ -63,14 +99,57 @@ public class AccountService {
     @Transactional
     public void physicalDeleteAccount(Long accountId) {
         Account account = findByAccountId(accountId);
+
+        // redis에서 삭제
+        String redisKey = ACCOUNT_CACHE_KEY + accountId;
+        redisTemplate.delete(redisKey);
+
+        // MySQL에서 삭제
         accountsRepository.delete(account);
+
+        // MongoDB에서 삭제
+        String _id = accountId.toString();
+        if(!accountDocumentRepository.existsById(_id)){
+            throw new BlogException(ExceptionCode.ACCOUNT_NOT_FOUND);
+        }
+        accountDocumentRepository.deleteById(_id);
     }
 
     // Redis에서 ID로 이메일 조회
+    @Transactional(readOnly = true)
+    public String findEmailByIdFromRedis(Long id) {
+        String redisKey = ACCOUNT_CACHE_KEY + id;
 
+        // Redis 해시에서 값 조회
+        Map<String, Object> hashEntries = hashOperations.entries(redisKey);
+        if (hashEntries.isEmpty()){
+            // DB에서 조회
+            Account account = findByAccountId(id);
+
+            // DB에서 조회한 정보를 Redis에 저장
+            hashOperations.put(redisKey, "email", account.getEmail());
+            hashOperations.put(redisKey, "nickname", account.getNickname());
+
+            // 만료 시간 설정(30분)
+            redisTemplate.expire(redisKey, 30, TimeUnit.MINUTES);
+
+            return account.getEmail();
+        }
+
+        return (String) hashEntries.get("email");
+
+    }
 
 
     // MongoDB에서 ID로 닉네임 조회
+    @Transactional(readOnly = true)
+    public String findNicknameByIdFromMongo(Long id){
+        String _id = id.toString();
+        AccountDocument accountDocument = accountDocumentRepository.findById(_id)
+                .orElseThrow(()-> new BlogException(ExceptionCode.ACCOUNT_NOT_FOUND));
+
+        return accountDocument.getNickname();
+    }
 
 
     /*----------------------------------------------------*/
